b3d788ee28ae669d50465555f2662fa30f5ac6e6
DhairyaG23 <dhairyag23@gmail.com>
2020-03-30 19:49:07 -0500

    updated and added files and modules

---
+++ app.js
@@ -0,0 +1,15 @@
+const request = require('request');
+
+
+setInterval(function(){
+request("https://pomber.github.io/covid19/timeseries.json", { json: true }, (err, res, body) => {
+  if (err) { 
+      return console.log(err); 
+  }
+  var i;
+  for(i=0; i < body["Argentina"].length; i++)
+
+  console.log(body.id);
+  console.log(body.title);
+});
+}, 10*1000);
