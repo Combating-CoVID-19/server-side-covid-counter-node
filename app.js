50786e4ebe25ca82530bb43921f4102e12385b3f
ccans <55026208+ccans@users.noreply.github.com>
2020-03-31 16:02:52 -0500

    Working fetch code

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const request = require('request')
 const admin = require('firebase-admin');
 
 
-var app = admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: 'https://combating-covid-19.firebaseio.com'
-});
+// var app = admin.initializeApp({
+//     credential: admin.credential.applicationDefault(),
+//     databaseURL: 'https://combating-covid-19.firebaseio.com'
+// });
 
-var database = app.database();
+// var database = app.database();
 
 // var database = firebase.database();
 
@@ -33,72 +33,62 @@ request("https://covid19.mathdro.id/api/countries/", {
     for (i = 0; body.countries.length > i; i++) {
         var currentCountryName = body.countries[i].name
         countryArray.push(currentCountryName);
+
     }
+console.log(countryArray);
 
-
-    // for(i=0; i < body.length; i++){
-    //     console.log(body.Argentina[i].date)
-    // }
-    // for (i in body) {
-
-    //     // countryArray.push(i);
-    //     // confirmedArray.push(i.confirmed)
-    //     // console.log(i[1].confirmed)
-    //     myObj = body;
-    //     for (x in myObj) {
-    //         console.log(x)
-    //         countryArray.push(x)
-
-
-    //     }
-    //     for (i = 0; countryArray.length > i; i++) {
-    //         var totalConfirmed = 0;
-    //         currentCountry = countryArray[i]
-    //         for (e = 0; myObj[currentCountry].length > e; e++) {
-    //             console.log(myObj[currentCountry][e].confirmed)
-    //             totalConfirmed = myObj[currentCountry][e].confirmed;
-    //             console.log(currentCountry + ' total confirmed ' + totalConfirmed)
-    //             arrayLength = myObj[currentCountry].length
-    //             newCases = (myObj[currentCountry][arrayLength - 1].confirmed) - (myObj[currentCountry][arrayLength - 2].confirmed);
-    //             console.log("New Cases " + newCases)
-    //         }
-    //     }
-
-    // }
-    // for(x=0; countryArray.length > x; x++){
-    //     var currentCountry = countryArray[x]
-    //     console.log(body.currentCountry)
-    //     // console.log(body[i])
-    // //  console.log(body.Argentina[x].confirmed);
-    // // console.log(countryArray);
-    // }
 
     //THIS LOOP IS TO GET THE TOTAL CONFIRMED CASES PER COUNTRY
-    for (x = 0; countryArray.length > x; x++) {
+    var confirmed = null;
+    var x = 0;
+    veryEpic();
+    function veryEpic() {
+    if(countryArray.length > x){
         currentCountry = countryArray[x];
-        console.log(currentCountry)
-        request('https://covid19.mathdro.id/api/countries/'+currentCountry, {
+
+        var url = 'https://covid19.mathdro.id/api/countries/'+currentCountry;
+
+        request(url, {
             json: true
         }, (err, res, body) => {
+            // console.log('running');
             if (err) {
                 return console.log(err)
             }
-            var confirmed = body.confirmed.value;
-            var recovered = body.recovered.value;
-            var deaths = body.deaths.value;
-            // writeCovidData(currentCountry, confirmed, recovered, deaths)
-            database.ref('CoVIDdata/' + currentCountry).set({
-                Country: currentCountry,
-                Confirmed: confirmed,
-                Recovered: recovered,
-                Deaths: deaths
-            });
+          if(body.confirmed == undefined) {
+            x++
+            veryEpic();
+          } else {
+          var confirmed = body.confirmed.value;
+          didEpicHappen();
+            function didEpicHappen() {
+                if(confirmed == null) {
+                    didEpicHappen();
+                } else {
+                  confirmed = null;
+                  // database.ref('CoVIDdata/' + currentCountry).set({
+                  //       Country: currentCountry,
+                  //       Confirmed: confirmed,
+                  //       Recovered: recovered,
+                  //       Deaths: deaths
+                  //   });
+                  x++;
+                  veryEpic();
+                }
+            }
+          }
 
-    
+
+
+
+
         });
 
+    } else {
+      console.log('done');
     }
 
+}
 });
 
 
