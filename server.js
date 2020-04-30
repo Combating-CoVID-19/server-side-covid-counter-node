dedc5d7171129340e45555fae75d9e32b3360cab
DhairyaG23 <dhairyag23@gmail.com>
2020-04-29 23:41:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ var app = admin.initializeApp({
 
 var database = app.database();
 
+console.log('Adding Data in 10 Minutes')
 // var database = firebase.database();
 
-var starCountRef = database.ref('/CoVIDdata/');
-starCountRef.on('value', function(snapshot) {
-  console.log(snapshot.val())
-});
+// var starCountRef = database.ref('/CoVIDdata/');
+// starCountRef.on('value', function(snapshot) {
+// //   console.log(snapshot.val())
+// });
 
 // database.ref('/CoVIDdata/Afghanistan/').once('value').then(function(snapshot) {
 //     var username = (snapshot.val());
@@ -22,6 +23,7 @@ starCountRef.on('value', function(snapshot) {
 //     // ...
 //   });
 setInterval(function(){
+
 var x;
 var i;
 var countryArray = [];
@@ -104,7 +106,7 @@ request("https://covid19.mathdro.id/api/countries/", {
                     var deaths = body.deaths.value;
                     // console.log('doing work')
 
-                    console.log(x)
+                    // console.log(x)
                     didEpicHappen();
 
                     function didEpicHappen() {
@@ -142,13 +144,13 @@ request("https://covid19.mathdro.id/api/countries/", {
                 }
 
                 // console.log(body.countries)
-                console.log(totalRecovered)
+                // console.log(totalRecovered)
                 var totals = 'Totals'
-                console.log(body.confirmed.value)
+                // console.log(body.confirmed.value)
                 totalConfirmed = body.confirmed.value;
                 totalRecovered = body.recovered.value;
                 totalDeaths = body.deaths.value;
-                console.log(totalDeaths)
+                // console.log(totalDeaths)
                 database.ref('CoVIDdata/' + totals).set({
                     Confirmed: totalConfirmed,
                     Recovered: totalRecovered,
@@ -165,7 +167,103 @@ request("https://covid19.mathdro.id/api/countries/", {
 });
 
 
-}, 10*1000);
+getCovidNews()
+function getCovidNews(){
+    var x = 0;
+    request("http://newsapi.org/v2/everything?q=coronavirus&sortBy=relevancy&language=en&apiKey=c82e65fa2c7b4873827205508123e2db", {
+        json: true
+    }, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        // console.log(body.articles)
+        addNewsData()
+        function addNewsData(){
+            if( x < body.articles.length){
+                var sourceID = body.articles[x].source.id;
+                var sourceName = body.articles[x].source.name;
+                var author = body.articles[x].author;
+                var title = body.articles[x].title;
+                var description = body.articles[x].description;
+                var url = body.articles[x].url;
+                var urlToImage = body.articles[x].urlToImage;
+                var publishDate = body.articles[x].publishedAt;
+                var content = body.articles[x].content;
+                console.log(title)
+                if(sourceID == null || sourceID == undefined){
+                    sourceID = 'undefined'
+                }
+                if(sourceName == null || sourceName == undefined){
+                    sourceName = "undefined"
+                }
+                if(author == null || author == undefined){
+                    author = "undefined"
+                }
+                if(title == null || title == undefined){
+                    title = "undefined"
+                }
+                if(description == null || description == undefined){
+                    description = "undefined"
+                }
+                if(url == null || url == undefined){
+                    url = "undefined"
+                }
+                if(urlToImage == null || urlToImage == undefined){
+                    urlToImage = "undefined"
+                }
+                if(publishDate == null || publishDate == undefined){
+                    publishDate = "undefined"
+                }
+                if(content == null || content == undefined){
+                    content = "undefined"
+                }
+                var earlyPath = title.replace('.', '')
+                var earlyPath2 = earlyPath.replace('#', "")
+                var earlyPath3 = earlyPath2.replace('$', '')
+                var earlyPath4 = earlyPath3.replace('[', '')
+                var earlyPath5 = earlyPath4.replace('.','')
+                var finalPath = earlyPath5.replace(']', '')
+                
+            
+                // console.log(finalPath)
+                // var earlyPath2 = earlyPath.split('.')
+                // var earlyPath3 = earlyPath2.split('#')
+                // var earlyPath4 = earlyPath3.split('$')
+                // var earlyPath5 = earlyPath4.split('[')
+                // var finalPath = earlyPath5.split(']')
+                // console.log(body.articles[x].author)
+                database.ref('News/' + finalPath).set({
+                SourceID: sourceID,
+                SourceName: sourceName,
+                Author: author,
+                Title: title,
+                Description: description,
+                URL: url,
+                URLToImage: urlToImage,
+                PublishDate: publishDate,
+                Content: content
+                
+                
+                });
+
+                x++;
+                addNewsData()
+            }
+        }
+        
+        //     Confirmed: totalConfirmed,
+        //     Recovered: totalRecovered,
+        //     Deaths: totalDeaths,
+        //     Deathrate: (((totalDeaths/totalConfirmed)*100).toFixed(8)),
+        //     DeathrateRounded: (((totalDeaths/totalConfirmed)*100).toFixed(2))
+        });
+
+ 
+}
+
+console.log('Adding Data in 10 Minutes')
+
+}, 60*1000*10);
 
 
 
@@ -177,3 +275,6 @@ function writeCovidData(country, confirmedCases, recoveredCases, deathCases) {
         Deaths: deathCases
     });
 }
+
+
+//http://newsapi.org/v2/everything?q=bitcoin&from=2020-03-30&sortBy=publishedAt&apiKey=c82e65fa2c7b4873827205508123e2db
